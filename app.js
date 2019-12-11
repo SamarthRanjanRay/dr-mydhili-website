382dd52ac4fef85139f46e6911003ddfc72a4b77
Kushal <kushalgarg2000@gmail.com>
2019-12-11 13:15:12 +0530

    Adding login page

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const passport = require("passport");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.set('view engine','ejs');
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.render("index");
+})
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+})
+
+app.post("/login",(req,res)=>{
+    console.log(req.body)    
 })
 
 app.listen(3000,()=>{
